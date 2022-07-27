c2db839d5d7493dd551f8f7443607260d2b6a4e2
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-07-27 00:38:16 -0400

    working on styling

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -3,6 +3,14 @@ import React from 'react';
 function About () {
     return (
         <section>
+                <div
+      style={{
+        backgroundColor: 'blue',
+        
+        width: '100px',
+        height: '100px'
+      }}
+    />
 
             <div className='center' id='about'>
                 <h1 className='page-header'>About Me</h1>
@@ -12,6 +20,8 @@ function About () {
                 Software engineer with the ability to find efficient solutions that make a large-scale impact. I have a proven track record of investigating and implementing new solutions and design systems, working with other industry experts to develop exceptional user experiences, and maintaining a keen eye for code reviews and sprint planning. I'm also able to adapt quickly to new environments, manage time effectively, and work on a wide range of business projects. Thanks for connecting!   
                 </p>
             </div>
+
+            
         </section>
     );
 }
--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -1,5 +1,8 @@
 import React from "react";
-import FoodRun from '../../assets/images/foodrun.png'
+import FoodRun from '../../assets/images/foodrun.png';
+import Seo from '../../assets/images/seo.png';
+// import neverbelate from '../../assets/images/neverbelate.png';
+
 
 function Portfolio () {
     return (
@@ -10,32 +13,32 @@ function Portfolio () {
         
         <div class='img-card'>
                 <h2>Food Run</h2>
-				<p>Interactive Full-Stack Application.</p>
+				<p>Interactive Full-Stack Application</p>
 					<img src={FoodRun} alt="Screenshot of foodrun game" />
-				<a href="https://dry-bastion-57106.herokuapp.com/login">
-					This is a link to the live site for this application
+				<a href="https://pacific-everglades-44013.herokuapp.com/">
+                Link to the live site application
 				</a>
-				<a href ="hhttps://github.com/untjala/food-run">This is a link to the repo for this application</a>
+				<a href ="hhttps://github.com/untjala/food-run">Link to Rep Application</a>
             </div>
 
             <div class='img-card'>
-                <h2>Food Run</h2>
-				<p>Interactive Full-Stack Application.</p>
-					<img src={FoodRun} alt="Screenshot of foodrun game" />
-				<a href="https://dry-bastion-57106.herokuapp.com/login">
-					This is a link to the live site for this application
+                <h2>Never Be Late</h2>
+				<p>Daily Planner Application</p>
+					{/* <img src={neverbelate} alt="Screenshot of planner" /> */}
+				<a href="https://mj-allen.github.io/neverbelate/">
+					Link to the live site application
 				</a>
-				<a href ="hhttps://github.com/untjala/food-run">This is a link to the repo for this application</a>
+				<a href ="https://github.com/MJ-Allen/neverbelate">Link to Rep Application</a>
             </div>
 
             <div class='img-card'>
-                <h2>Food Run</h2>
-				<p>Interactive Full-Stack Application.</p>
-					<img src={FoodRun} alt="Screenshot of foodrun game" />
-				<a href="https://dry-bastion-57106.herokuapp.com/login">
-					This is a link to the live site for this application
+                <h2>SEO</h2>
+				<p>SEO Refactor Application.</p>
+					<img src={Seo} alt="Screenshot of application" />
+				<a href="https://mj-allen.github.io/seo-refactor/">
+                Link to the live site application
 				</a>
-				<a href ="hhttps://github.com/untjala/food-run">This is a link to the repo for this application</a>
+				<a href ="https://github.com/MJ-Allen/seo-refactor">Link to Rep Application</a>
             </div>
 
          </section>
